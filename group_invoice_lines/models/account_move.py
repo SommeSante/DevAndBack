752896d46b399c6607819703eb7c9c0b4d4eb5cf
LFer <lferreira@proyectasoft.com>
2021-10-14 17:24:43 -0300

    se agregaron los campos de codigo de producto y codigo de unidad de medida al reporte de facturas

--- group_invoice_lines/models/account_move.py
+++ group_invoice_lines/models/account_move.py
@@ -11,12 +11,15 @@ class AccountMove(models.Model):
         invoice_lines = self.invoice_line_ids.sorted(key=lambda l: (-l.sequence, l.date, l.move_name, -l.id), reverse=True)
         lines_dict = {}
         for line in invoice_lines:
+
             if line.product_id.id not in lines_dict:
                 lines_dict[line.product_id.id] = [
+                    line.product_id.unspsc_code_id.code if line.product_id.unspsc_code_id else '',
                     line.name,
                     line.quantity,
                     line.product_uom_id.name,
                     line.price_unit,
+                    line.product_uom_id.unspsc_code_id.code if line.product_uom_id.unspsc_code_id else '',
                     line.discount,
                     line.tax_ids,
                     line.price_subtotal,
@@ -25,10 +28,10 @@ class AccountMove(models.Model):
                 ]
 
             else:
-                lines_dict[line.product_id.id][1] += line.quantity
-                lines_dict[line.product_id.id][4] += line.quantity
-                lines_dict[line.product_id.id][6] += line.price_subtotal
-                lines_dict[line.product_id.id][7] += line.price_total
+                lines_dict[line.product_id.id][2] += line.quantity
+                # lines_dict[line.product_id.id][4] += line.quantity
+                lines_dict[line.product_id.id][8] += line.price_subtotal
+                lines_dict[line.product_id.id][9] += line.price_total
         for value in lines_dict:
             lines.append(lines_dict[value])
 
