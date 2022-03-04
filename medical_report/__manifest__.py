0c38565bef51de2493781cba20d7400285894c16
divya vyas <divyapanchal7594@gmail.com>
2022-03-04 16:43:48 +0530

    se agrego el modulo

---
+++ medical_report/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Medical Report',
+    'author': "Divya",
+    'version': "14.0",
+    'description': "Medical Report",
+    'depends': ['base'],
+    'data': [
+        'views/examen_medico_report.xml',
+         #'views/report.xml',
+       ],
+    'qweb': [
+
+    ],
+    'application': False,
+    'installable': True,
+}
