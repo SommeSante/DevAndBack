d621ca2bb4f5af6505d1379125baeb6a7a10b397
divya vyas <divyapanchal7594@gmail.com>
2022-03-17 19:14:11 +0530

    added new category for service medical group

---
+++ custom_sommesante/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Custom Sommesante',
+    'author': "Divya",
+    'version': "14.0",
+    'description': "customization of sommesante project",
+    'depends': ['base'],
+    'data': [
+       'security/service_medical_security.xml',
+       ],
+    'qweb': [
+
+    ],
+    'application': False,
+    'installable': True,
+}
