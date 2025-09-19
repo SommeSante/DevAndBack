8b256c68aeee379dc00281f92d518ec308522163
CarlosAlt <48330309+CarlosAlt@users.noreply.github.com>
2025-09-19 17:43:01 -0600

    Update __manifest__.py GIL

--- group_invoice_lines/__manifest__.py
+++ group_invoice_lines/__manifest__.py
@@ -8,14 +8,14 @@
     """,
     'author': "Somme Sante TI",
     'website': "https://sommesante.com",
-    'category': 'Accounting/Accounting',
+    'category': 'Accounting',
     'version': '18.0.1.0.0',
-    'depends': ['account_accountant', 'web_enterprise'],
+    'depends': ['account_accountant'],
 
     'data': [
         'views/report_invoice.xml',
     ],
     'installable': True,
     'application': False,
-    'license': 'OEEL-1',
+    'license': 'LGPL-3',
 }
