4354cca7b69382ef4b303e53fe4e185938a37872
Carlos Daniel Altamira García <sistemas@sommesante.com>
2025-10-03 11:58:59 -0600

    update-odoo18

--- bitacora_consultas/__manifest__.py
+++ bitacora_consultas/__manifest__.py
@@ -1,31 +1,23 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "bitacora consultas",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
+    'name': "Bitácora de Consultas",
+    'summary': "Registro de consultas médicas o administrativas",
     'description': """
-        Long description of module's purpose
+        Este módulo permite llevar un control y bitácora de las consultas,
+        almacenando información relevante para seguimiento y reportes.
     """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
+    'author': "Tu Empresa",
+    'website': "https://www.tuempresa.com",
+    'category': 'Extra Tools',
+    'version': '18.0.1.0.0',
     'depends': ['base'],
-
-    # always loaded
     'data': [
-        
+        'security/ir.model.access.csv',
+        'views/bitacora_consulta_views.xml',
+        # 'views/menus.xml',   # opcional si quieres menú propio
     ],
-    # only loaded in demonstration mode
-    # 'demo': [],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
 }
